844a9b1ba5a02994712a77b600ee0e6bdbc1d3fe
QUANG-SanBay <quangsanbay@gmail.com>
2025-06-14 11:58:52 +0700

    comment lại file js

--- assets/js/CV_KTuyen.js
+++ assets/js/CV_KTuyen.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sections = document.querySelectorAll(".section");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const sections = document.querySelectorAll(".section");
 
-    sections.forEach((section, index) => {
-        section.style.opacity = 0;
-        section.style.transition = `opacity 1s ease ${index * 0.3}s, transform 1s ease ${index * 0.3}s`;
+//     sections.forEach((section, index) => {
+//         section.style.opacity = 0;
+//         section.style.transition = `opacity 1s ease ${index * 0.3}s, transform 1s ease ${index * 0.3}s`;
 
-        setTimeout(() => {
-            section.style.opacity = 1;
-            section.style.transform = "translateY(0)";
-        }, index * 300 + 500);
-    });
-});
+//         setTimeout(() => {
+//             section.style.opacity = 1;
+//             section.style.transform = "translateY(0)";
+//         }, index * 300 + 500);
+//     });
+// });
 
 
 
