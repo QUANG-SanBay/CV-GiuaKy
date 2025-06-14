2b327c5444093a81c8f04c9f2d3e237738de1daf
QUANG-SanBay <quangsanbay@gmail.com>
2025-06-14 09:55:20 +0700

    update UI 2 Thành viên

---
+++ assets/js/CV_KTuyen.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+
+    sections.forEach((section, index) => {
+        section.style.opacity = 0;
+        section.style.transition = `opacity 1s ease ${index * 0.3}s, transform 1s ease ${index * 0.3}s`;
+
+        setTimeout(() => {
+            section.style.opacity = 1;
+            section.style.transform = "translateY(0)";
+        }, index * 300 + 500);
+    });
+});
+
+
+
+
+
